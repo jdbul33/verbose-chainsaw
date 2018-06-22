b35888a6fa2c5577384c50864ae3277c7526b249
John Bulger <35578045+jdbul33@users.noreply.github.com>
2018-06-21 22:28:50 -0400

    set up

--- South_Bend_311/Beginnings.py
+++ South_Bend_311/Beginnings.py
@@ -12,10 +12,41 @@ Line, using publicly available data from
 https://data-southbend.opendata.arcgis.com
 
 """
-
+#%%
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+# need to import the rest of bokeh here
+
+"""
+Importing the data files, stored locally on machine but available through GitHub
+"""
 
 file = "C://users/jdbul/Documents/GitHub/Verbose-Chainsaw/311_Contact_Management_Cases.csv"
 file2 = "C://users/jdbul/Documents/GitHub/Verbose-Chainsaw/311_Call_Center_Activity_by_Day.csv"
+file3 = "C://users/jdbul/Google Drive/Python Independent Study/311 Calls 092916_061518_scrub.csv"
 case_data = pd.read_csv(file)
 daily_data = pd.read_csv(file2)
+article_data = pd.read_csv(file3)
+
+#%%
+"""
+Evaluating basic data tidiness
+"""
+
+print(case_data.columns)
+print(case_data.info())
+print(case_data.describe())
+#%%
+print(daily_data.columns)
+print(daily_data.info())
+print(daily_data.describe())
+#%%
+print(article_data.columns)
+print(article_data.info())
+print(article_data.describe())
+
+#%%
+"""
