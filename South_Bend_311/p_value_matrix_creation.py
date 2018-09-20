59cd9eb213cd6ab52c3a1c26c2aab1b8fbe019af
John Bulger <35578045+jdbul33@users.noreply.github.com>
2018-09-20 10:04:50 -0500

    working on hist

--- South_Bend_311/p_value_matrix_creation.py
+++ South_Bend_311/p_value_matrix_creation.py
@@ -32,4 +32,59 @@ p_df = p_df.apply(pd.to_numeric)
 d = p_df.where(np.tril(np.ones(p_df.shape)).astype(np.bool))
 fig = sns.heatmap(d, center = .05, cmap='coolwarm')
 plot = fig.get_figure()
-plot.savefig('Heatmap.png')
+#plot.savefig('Heatmap.png')
+
+
+#%%
+"""
+reuse code from bokeh to get call distributions
+"""
+df4 = topics
+df4['Date'] = article_data['Created On']
+df4['Date'] = pd.to_datetime(df4['Date'], format='%m/%d/%Y %H:%M')
+df4['Date'] = df4['Date'].apply(lambda x: x.date())
+
+day = []
+for i in range(len(df4['Date'])):
+    x = df4.iloc[i]['Date']
+    x = x.weekday()+2
+    day.append(x)
+df4['Day_of_Week'] = day
+
+
+temp1 = daily_data[['Day_of_Week', 'CallsPresented']].copy()
+temp1 = temp1.reset_index(drop=True)
+temp1.rename(columns={'CallsPresented': 'Count'}, inplace=True)
+df4_1 = df4.groupby(['Date', 'Day_of_Week'], as_index=False)['Count'].count()
+df4_1 = df4_1.drop(columns = ['Date'])
+
+new_df = pd.concat([temp1, df4_1], ignore_index=True)
+
+mon = np.array(new_df.Count[new_df['Day_of_Week'] == 2])
+tue = np.array(new_df.Count[new_df['Day_of_Week'] == 3])
+wed = np.array(new_df.Count[new_df['Day_of_Week'] == 4])
+thu = np.array(new_df.Count[new_df['Day_of_Week'] == 5])
+fri = np.array(new_df.Count[new_df['Day_of_Week'] == 6])
+
+#%%
+sns.set()
+
+hist_dict = {}
+
+for data, name in zip([mon, tue, wed, thu, fri], ['Monday','Tuesday','Wednesday','Thursday','Friday']):
+    plt.figure()
+    plt.title("Call Distribution for " + name)
+    plt.xlabel("Number of Daily Calls")
+    plt.ylabel("Number of Days")
+    plt.xlim(0, 1200)
+    plt.hist(data, bins=24)
+    
+#%%
+import pylab
+import matplotlib._pylab_helpers
+
+figures=[manager.canvas.figure
+         for manager in matplotlib._pylab_helpers.Gcf.get_all_fig_managers()]
+print(figures)
+
+plt.get_fignums():
