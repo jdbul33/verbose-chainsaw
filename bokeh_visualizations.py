302e0eeee844ee27583ff390d5df7018566f28a6
John Bulger <35578045+jdbul33@users.noreply.github.com>
2018-10-14 17:00:51 -0400

    added new plot

--- bokeh_visualizations.py
+++ bokeh_visualizations.py
@@ -8,12 +8,14 @@ Created on Thu Jul 12 12:43:26 2018
 import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, output_file, show
-from bokeh.layouts import row, column
+from bokeh.layouts import row, column, widgetbox
 from math import pi
 from bokeh.transform import cumsum
 from bokeh.palettes import Category20, brewer, Viridis, Magma, Category10
 from bokeh.models import Jitter, Whisker, ColumnDataSource, NumeralTickFormatter
 from scipy.stats import sem
+from bokeh.models.widgets import RangeSlider
+from bokeh.models.callbacks import CustomJS
 #%%
 """
 Create Pie Chart Bokeh visualization for total call time by dept
@@ -264,6 +266,153 @@ p6.xaxis.minor_tick_line_color = None
 p6.xaxis.major_label_text_font_size = "11pt"
 p6.yaxis.major_label_text_font_size = "11pt"
 
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 11:25:39 2018
+
+@author: jdbul
+"""
+
+import pandas as pd
+import numpy as np
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import row, column, widgetbox
+from math import pi
+from bokeh.transform import cumsum
+from bokeh.palettes import Category20, brewer, Viridis, Magma, Category10
+from bokeh.models import Jitter, Whisker, ColumnDataSource, NumeralTickFormatter
+from scipy.stats import sem
+from bokeh.models.widgets import RangeSlider
+from bokeh.models.callbacks import CustomJS
+
+#%%
+"""
+retooling Jitterplot code to make scatter plot with sliders, clickable legend, etc
+"""
+
+df_6 = topic_data
+
+dept_name = list(df_6['KB_Article'])
+
+for i in range(len(dept_name)):
+    n = dept_name[i]
+    dept_name[i] = n.replace(' - KB Team', '')
+    
+df_6['Dept'] = dept_name
+
+
+df_6 = df_6.loc[(df_6['Dept'] == 'Water Works') |
+        (df_6['Dept'] == 'Solid Waste') |
+        (df_6['Dept'] == 'Streets') |
+        (df_6['Dept'] == 'Code Enforcement') |
+        (df_6['Dept'] == "Mayor's Office") |
+        (df_6['Dept'] == 'Engineering') |
+        (df_6['Dept'] == 'Animal Control')]
+
+df_6.reset_index(drop=True, inplace=True)
+
+df_6 = df_6.drop(df_6[df_6.Count < 20].index)
+
+df_6.reset_index(drop=True, inplace=True)
+
+#df_6.sort_values(by='Count', ascending=False, inplace=True)
+
+colors = brewer['Set1'][7]
+p8 = figure(title = 'Average Duration and Number of Calls by Topic in Top Departments \n 9/2016 - 6/2018', x_axis_label = "Average Duration in Seconds", 
+            y_axis_label = 'Total Number of Calls by Topic',
+             tools=["hover", 'reset', 'save'], tooltips="@Topic; average duration of @Seconds seconds; @Count total calls")
+
+for i, d in enumerate(list(df_6['Dept'].unique())):
+    y = df_6[df_6['Dept'] == d][['Count', 'Seconds', 'Topic']]
+    color = colors[i  % len(colors)]
+    if d == 'Water Works':
+        p8.circle(x='Seconds', y='Count', source=y, color=color, size=12, alpha=0.75, legend= d)
+    elif d == 'Solid Waste':
+        p8.diamond(x='Seconds', y='Count', source=y, color=color, size=12, alpha=0.75, legend= d)
+    elif d == 'Streets':
+        p8.asterisk(x='Seconds', y='Count', source=y, color=color, size=12, alpha=0.75, legend= d)
+    elif d == 'Code Enforcement':
+        p8.square(x='Seconds', y='Count', source=y, color=color, size=12, alpha=0.75, legend= d)
+    elif d == "Mayor's Office":
+        p8.inverted_triangle(x='Seconds', y='Count', source=y, color=color, size=12, alpha=0.75, legend= d)
+    elif d == 'Engineering':
+        p8.x(x='Seconds', y='Count', source=y, color=color, size=12, alpha=0.75, legend= d)
+    elif d == 'Animal Control':
+        p8.circle_cross(x='Seconds', y='Count', source=y, color=color, size=12, alpha=0.75, legend= d)
+   
+
+p8.xaxis.major_label_orientation = pi/3
+p8.xaxis.major_tick_line_color = None
+p8.xaxis.minor_tick_line_color = None
+p8.xaxis.major_label_text_font_size = "11pt"
+p8.yaxis.major_label_text_font_size = "11pt"
+
+p8.legend.location = "top_right"
+p8.legend.click_policy="hide"
+
+
+#%%
+"""
+Adding y slider
+"""
+
+callback = CustomJS(args=dict(yr=p8.y_range), code="""
+
+// JavaScript code goes here
+
+var a = cb_obj.value[0];
+
+// the model that triggered the callback is cb_obj:
+var b = cb_obj.value[1];
+
+// models passed as args are automagically available
+yr.start = a;
+yr.end = b;
+
+""")
+    
+y_range_slider = RangeSlider(start=0, end=13000, value=(0,13000), step=20, title="Zoom by Number of Total Calls",
+                              callback_policy='mouseup')
+
+
+y_range_slider.js_on_change('value', callback) 
+
+
+
+
+#%%
+"""
+Adding x slider
+"""
+
+callback = CustomJS(args=dict(xr=p8.x_range), code="""
+
+// JavaScript code goes here
+
+var a = cb_obj.value[0];
+
+// the model that triggered the callback is cb_obj:
+var b = cb_obj.value[1];
+
+// models passed as args are automagically available
+xr.start = a;
+xr.end = b;
+
+""")
+    
+x_slider = RangeSlider(start=0, end=400, value=(0,400), step=10, title="Zoom by Average Call Duration",
+                        callback_policy='mouseup')
+
+
+x_slider.js_on_change('value', callback) 
+
+#p6.x_range.js_on_change('start', callback)
+   
+
+
+
+
+
 #%%
 """
 Create a gridplot layout and a shareable HTML file
@@ -274,6 +423,7 @@ This will effectively be a dashboard that will be interactive
 g = row(p, p2)
 h = row(p4, p5)
 j = row(p6, p3)
-output_file('311_Call_Center_Dashboard.html', title='311 Call Center Dashboard')
-show(column(g,h,j))
+k = row(p8, widgetbox(y_range_slider, x_slider))
+output_file('311_Call_Center_Dashboard_Modified.html', title='311 Call Center Dashboard')
+show(column(g,h,j,k))
 
